39201c13bfb4c5cd48220d998b7e24ed27e4aa4c
Anthony Pereira <60923555+Anth0nyPereira@users.noreply.github.com>
2022-08-09 15:06:07 +0100

    created a dissolve shader graph material and animation

---
+++ magic-jelly-beans/Assets/Scripts/AnimateDissolve.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimateDissolve : MonoBehaviour
+{
+    private Animator animator;
+    
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            //animator.SetTrigger("makeDissolve");
+        }
+        
+        
+
+    }
+}
