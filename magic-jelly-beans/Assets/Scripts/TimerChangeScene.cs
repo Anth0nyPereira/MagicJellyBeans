c8cb28143f6bf4642ecc881f49a0fb2220add7e8
Anthony Pereira <60923555+Anth0nyPereira@users.noreply.github.com>
2022-09-05 22:25:12 +0100

    changing scene from intro to main menu

---
+++ magic-jelly-beans/Assets/Scripts/TimerChangeScene.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TimerChangeScene : MonoBehaviour
+{
+    [SerializeField]
+    private float seconds;
+
+    [SerializeField]
+    private string nameOfNextScene;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > seconds)
+        {
+            changeScene();
+        }
+    }
+
+    private void changeScene()
+    {
+        SceneManager.LoadScene(nameOfNextScene, LoadSceneMode.Single);
+    }
+}
